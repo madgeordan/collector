68707290b97d85f37d0bc8942583a3b7ce45da17
George Lushch <madgeordan@gmail.com>
2017-06-16 17:27:14 +0300

    imagepickercontroller is done

---
+++ collector/CreateVC.swift
@@ -0,0 +1,38 @@
+//
+//  CreateVC.swift
+//  collector
+//
+//  Created by George Lushch on 6/16/17.
+//  Copyright © 2017 madgeordan. All rights reserved.
+//
+
+import UIKit
+
+class CreateVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBAction func cameraButton(_ sender: Any) {
+        print("cam")
+    }
+    
+    
+    @IBAction func addButton(_ sender: Any) {
+        print("add")
+    }
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+    }
+    @IBAction func photosButton(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
